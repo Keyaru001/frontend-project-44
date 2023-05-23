b2ca1baa47eb54611f6304b7f51cd163effa5740
Фарит <faritkashaev2004@gmail.com>
2023-05-23 12:02:51 +0300

    finish 8

---
+++ src/games/brain-progression.js
@@ -0,0 +1,24 @@
+import { getRandom, generateMassive } from '../utils.js'
+
+function startEvent () {
+  const result = generateMassive()
+  const validAnswer = `${result[getRandom(result.length - 1)]}`
+  let text = ''
+  for (let i = 0; i < result.length; i += 1) {
+    if (i === 0) {
+      if (`${result[i]}` === validAnswer) {
+        text += '..'
+      } else {
+        text += `${result[i]}`
+      }
+    } else if (`${result[i]}` === validAnswer) {
+      text += ' ..'
+    } else {
+      text += ` ${result[i]}`
+    }
+  }
+  const quest = text
+  return [quest, validAnswer]
+}
+const lable = 'What number is missing in the progression?'
+export default { startEvent, lable }
--- src/gpicker.js
+++ src/gpicker.js
@@ -1,7 +1,8 @@
 import even from './games/brain-even.js'
 import calc from './games/brain-calc.js'
 import gcd from './games/brain-gcd.js'
+import progression from './games/brain-progression.js'
 
 export default {
-  even, calc, gcd
+  even, calc, gcd, progression
 }
