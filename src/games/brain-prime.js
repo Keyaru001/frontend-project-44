2b25f1902fc0efdea6c105f1733fac057e9aff10
Фарит <faritkashaev2004@gmail.com>
2023-05-23 12:04:45 +0300

    finish 9

---
+++ src/games/brain-prime.js
@@ -0,0 +1,11 @@
+import { getRandom, isPrime } from '../utils.js'
+
+function startEvent () {
+  const result = getRandom(100)
+  const validAnswer = isPrime(result) ? 'yes' : 'no'
+  const quest = `${result}`
+  return [quest, validAnswer]
+}
+
+const lable = 'Answer "yes" if given number is prime. Otherwise answer "no"'
+export default { startEvent, lable }
--- src/gpicker.js
+++ src/gpicker.js
@@ -2,7 +2,8 @@ import even from './games/brain-even.js'
 import calc from './games/brain-calc.js'
 import gcd from './games/brain-gcd.js'
 import progression from './games/brain-progression.js'
+import prime from './games/brain-prime.js'
 
 export default {
-  even, calc, gcd, progression
+  even, calc, gcd, progression, prime
 }
